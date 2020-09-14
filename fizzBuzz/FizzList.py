2ea9e7a5af47a5542072f598deda0634cd75e1d0
hsmith <hal.smith@altran.com>
2020-09-14 15:56:15 +0100

    Fully working with user input.

--- fizzBuzz/FizzList.py
+++ fizzBuzz/FizzList.py
@@ -1,43 +1,21 @@
+from fizzBuzz import FizzObject
+
+
 class FizzList:
-    def __init__(self, x):
-        self.eff_list = []
-        self.bee_list = []
+    def __init__(self):
+        self.object_list = []
         self.reverse = False
-        self.x = x
 
-    def add_object(self, rule_name):
-        if rule_name[0] == "F":
-            self.eff_list.append(rule_name)
-        elif rule_name[0] == "B":
-            self.bee_list.append(rule_name)
-        elif rule_name == "Reverse":
-            self.reverse = True
-
-    def contains(self, name: str):
-        if name in self.eff_list:
-            return True
-        if name in self.bee_list:
-            return True
-        return False
+    def add_object(self, fizz_object: FizzObject):
+        self.object_list.append(fizz_object)
 
     def format_output(self):
-        output_list = []
-        output_list += self.eff_list + self.bee_list
+        if any(fizz_object.delete_others for fizz_object in self.object_list):
+            self.object_list.remove(any(not fizz_object.permanent) for fizz_object in self.object_list)
 
-        if self.contains("Bong"):
-            output_list = []
-            if self.contains("Fezz"):
-                output_list.append("Fezz")
-            output_list.append("Bong")
+        self.object_list = sorted(self.object_list, key=lambda fizz_object_2: fizz_object_2.priority)
+        if self.reverse:
+            self.object_list.reverse()
+        names = (fizz_object.name for fizz_object in self.object_list)
 
-        output = ""
-        if len(output_list) > 0:
-            if self.reverse:
-                output_list.reverse()
-                output += "".join(output_list)
-            else:
-                output += "".join(output_list)
-        else:
-            return str(self.x)
-
-        return output
+        return "".join(names)
--- fizzBuzz/__init__.py
+++ fizzBuzz/__init__.py
@@ -1,29 +1,42 @@
 from fizzBuzz.FizzList import FizzList
+from fizzBuzz.FizzObject import FizzObject
 
 
 def multiple_of(dividend, divisor):
     return dividend % divisor == 0
 
 
-def fizz_buzz():
+def list_contains_string(parent_list, string_to_match):
+    return any(entry == string_to_match for entry in parent_list)
+
+
+def fizz_buzz(user_input_line):
+
+    input_entries = user_input_line.split(",")
 
     for x in range(1, 301):
-        fizz_list = FizzList(x)
+        fizz_list = FizzList()
 
-        if multiple_of(x, 3):
-            fizz_list.add_object("Fizz")
-        if multiple_of(x, 5):
-            fizz_list.add_object("Buzz")
-        if multiple_of(x, 7):
-            fizz_list.add_object("Bang")
-        if multiple_of(x, 11):
-            fizz_list.add_object("Bong")
-        if multiple_of(x, 13):
-            fizz_list.add_object("Fezz")
-        if multiple_of(x, 17):
-            fizz_list.add_object("Reverse")
+        if list_contains_string(input_entries, str(3)) and multiple_of(x, 3):
+            fizz_list.add_object(FizzObject("Fizz", False, False, 1))
 
+        if list_contains_string(input_entries, str(5)) and multiple_of(x, 5):
+            fizz_list.add_object(FizzObject("Buzz", False, False, 3))
+
+        if list_contains_string(input_entries, str(7)) and multiple_of(x, 7):
+            fizz_list.add_object(FizzObject("Bang", False, False, 4))
+        if list_contains_string(input_entries, str(11)) and multiple_of(x, 11):
+            fizz_list.add_object(FizzObject("Bong", True, True, 5))
+
+        if list_contains_string(input_entries, str(13)) and multiple_of(x, 13):
+            fizz_list.add_object(FizzObject("Fezz", True, False, 2))
+
+        if list_contains_string(input_entries, str(17)) and multiple_of(x, 17):
+            fizz_list.reverse = True
+            
         print("x = " + str(x) + ": " + fizz_list.format_output())
 
 
-fizz_buzz()
+user_input = input("Enter only integers separated by commas: ")
+print(user_input)
+fizz_buzz(user_input)
